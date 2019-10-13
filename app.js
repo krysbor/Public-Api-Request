8f24750d003e17d7acbfdd4993380f43d5369830
Krystian <vexiseffects@gmail.com>
2019-10-13 17:29:27 +0200

    add generateUserData and getData functions

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log('js connected')
+const URL = 'https://randomuser.me/api/?results=12'
 
 const displaySearchMarkup = () => {
     const searchContainer = document.querySelector('.search-container')
@@ -12,3 +13,32 @@ const displaySearchMarkup = () => {
     `
 }
 displaySearchMarkup()
+
+
+const generateUser = (object) => {
+    const gallery = document.querySelector('#gallery')
+    const div1 = document.createElement('div')
+    div1.className = 'card'
+    div1.innerHTML = `
+        <div class="card-img-container">
+            <img class="card-img" src="${object.picture.large}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${object.name.first} ${object.name.last} last</h3>
+            <p class="card-text">${object.email}</p>
+            <p class="card-text cap">${object.location.city}, ${object.location.state}</p>
+        </div>
+    `
+    gallery.appendChild(div1)
+
+}
+
+
+
+/* Downloads data */
+const getData = (url) => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {data.results.forEach(e => generateUser(e))})
+}
+getData(URL)
